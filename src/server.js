b41dc4bdc08a8174d37cc73d22205c4bfca67966
MuteenK <kundangarmuteen@protonmail.com>
2022-10-04 23:34:48 +0530

    Added Client Connection

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const http = require("http");
+const path = require('path')
+const {Server} = require("socket.io")
+
+const app = express();
+const port = 8000 || process.env.port
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.set('view engine', "ejs");
+app.use('/static', express.static(path.join(__dirname, '../public')))
+
+
+app.get('/', (req, res) => {
+    res.status(200).render('index.ejs');
+});
+
+
+io.on("connection", (socket) => {
+    console.log('New User Connected -> ' + socket.id );
+    socket.on("disconnect", () => {
+        console.log("User Disconnected")
+    })
+});
+
+
+server.listen(port, () => {
+    console.log(`Serving at http://127.0.0.1:${port}`);
+})
+
