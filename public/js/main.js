5d77858215abf69bc092445dfc96b66b6d5a357e
MuteenK <kundangarmuteen@protonmail.com>
2022-10-05 18:52:03 +0530

    Delete Message on Double click

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,13 @@
-var socket = io();
+var socket = io(); // Init Socket
 
+// Elements
 const box = document.getElementById("msg");
 const btn = document.getElementById("send");
 const msgArea = document.getElementById("text-msgs");
 const form = document.getElementById("form-id");
 
+
+// Local Storage Init
 if (localStorage.getItem("msgs") === null){
     localStorage.setItem("msgs", "");
 }
@@ -12,19 +15,35 @@ else{
     msgArea.innerHTML = localStorage.getItem("msgs");   
 }
 
+// When Form is submitted
 form.addEventListener("submit", e => {
     e.preventDefault();
+
+    // Sending Message to Server
     socket.emit("message", box.value);
-    msgArea.innerHTML += `<li class="my-msg"><span>${box.value}</span></li>`
+
+    // Displaying text manually
+    msgArea.innerHTML += `<li class="my-msg" ondblclick="deleteMsg(this)"><span>${box.value}</span></li>`
+
     localStorage.setItem("msgs", msgArea.innerHTML);
     box.value = "";
+
+    // Scroll to recent msg
     window.scrollTo(0, document.body.scrollHeight);
 })
 
+
+// On receiving message 
 socket.on("message", msg => {
-    msgArea.innerHTML += `<li class="others-msg"><span>${msg}</span></li>`;
+    msgArea.innerHTML += `<li class="others-msg" ondblclick="deleteMsg(this)"><span>${msg}</span></li>`;
     localStorage.setItem("msgs", msgArea.innerHTML);
     window.scrollTo(0, document.body.scrollHeight);
 })
 
+
+// To Delete a msg
+function deleteMsg(e) {
+    e.remove();
+    localStorage.setItem("msgs", msgArea.innerHTML);
+}
 
--- src/server.js
+++ src/server.js
@@ -1,23 +1,29 @@
+// Modules
 const express = require("express")
 const http = require("http");
 const path = require('path')
 const {Server} = require("socket.io")
 
+
+
+// Configuration
 const app = express();
 const port = 8000 || process.env.port
-
 const server = http.createServer(app);
 const io = new Server(server);
-
 app.set('view engine', "ejs");
 app.use('/static', express.static(path.join(__dirname, '../public')))
 
 
+
+// End Points
 app.get('/', (req, res) => {
     res.status(200).render('index.ejs');
 });
 
 
+
+// Socket Listener
 io.on("connection", (socket) => {
     console.log('New User Connected -> ' + socket.id );
     socket.on("disconnect", () => {
@@ -29,6 +35,7 @@ io.on("connection", (socket) => {
 });
 
 
+// Server
 server.listen(port, () => {
     console.log(`Serving at http://127.0.0.1:${port}`);
 })
