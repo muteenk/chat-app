c623ee8919b0a97738b7687547149162cc2e2776
MuteenK <kundangarmuteen@protonmail.com>
2022-10-05 18:16:30 +0530

    Chats are being saved in local storage

--- public/js/main.js
+++ public/js/main.js
@@ -5,17 +5,25 @@ const btn = document.getElementById("send");
 const msgArea = document.getElementById("text-msgs");
 const form = document.getElementById("form-id");
 
+if (localStorage.getItem("msgs") === null){
+    localStorage.setItem("msgs", "");
+}
+else{
+    msgArea.innerHTML = localStorage.getItem("msgs");   
+}
+
 form.addEventListener("submit", e => {
     e.preventDefault();
     socket.emit("message", box.value);
     msgArea.innerHTML += `<li class="my-msg"><span>${box.value}</span></li>`
+    localStorage.setItem("msgs", msgArea.innerHTML);
     box.value = "";
     window.scrollTo(0, document.body.scrollHeight);
 })
 
-
 socket.on("message", msg => {
     msgArea.innerHTML += `<li class="others-msg"><span>${msg}</span></li>`;
+    localStorage.setItem("msgs", msgArea.innerHTML);
     window.scrollTo(0, document.body.scrollHeight);
 })
 
